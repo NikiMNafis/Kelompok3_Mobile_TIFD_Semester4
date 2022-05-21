5536dd696243319a2c879618518be39791cbe6a8
Nalendraa <shawnaxcel42@gmial.com>
2022-05-21 22:34:08 +0700

    add assets & theme

---
+++ lib/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 30.0;
+
+Color primaryColor = Color(0xFF2BA651);
+Color secondaryColor = Color(0xFF0093A0);
+Color alertColor = Color(0xFFFA62B80);
+Color priceColor = Color(0xFF2c96f1);
+Color backgroundColor1 = Color(0xFF1F1D2B);
+Color backgroundColor2 = Color(0xFF2B2937);
+Color backgroundColor3 = Color(0xFF242231);
+Color primaryTextColor = Color(0xFFE1E1E1);
+Color secondaryTextColor = Color(0xFF999999);
+Color subtitleColor = Color(0xFF504f5e);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor,
+);
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: priceColor,
+);
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
