d6759d23d29c9edc190ba45b2ae32fa148bce909
Niki Muhammad Nafis <nikinafis08@gmail.com>
2022-05-23 20:14:23 +0700

    add splash screen adn sign in screen

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:project_akhir_mobile_smtr4/theme.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/sign-in'),
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: Center(
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                  'assets/image_splash.png',
+                ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
