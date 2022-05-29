25d6826621aaed635d4fabde7a9610a778d9db70
alfa1404 <amrezaalfarit@gmail.com>
2022-05-29 17:34:29 +0700

    Home screen bagian header

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -2,14 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:project_akhir_mobile_smtr4/theme.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text(
-          'Home Screen',
-          style: primaryTextStyle,
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
         ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Halo, Jokowi',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    '@alexkeinn',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                width: 54,
+                height: 54,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/image_profile.png',
+                    ),
+                  ),
+                )),
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
